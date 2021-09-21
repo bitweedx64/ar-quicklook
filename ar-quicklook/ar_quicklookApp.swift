7861bfaa251c842abd30af1c0b5c6d66f3d71f08
abnernat <abnernat@outlook.com>
2021-09-21 13:10:15 +0800

    Initial Commit

---
+++ ar-quicklook/ar_quicklookApp.swift
@@ -0,0 +1,17 @@
+//
+//  ar_quicklookApp.swift
+//  ar-quicklook
+//
+//  Created by Shay Zhang on 2021/9/21.
+//
+
+import SwiftUI
+
+@main
+struct ar_quicklookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
